0e6bce5036b8f39bd28d606bf57a4b5a72c4163e
Masum Raza <141407109+masum786786@users.noreply.github.com>
2025-05-27 20:46:23 +0530

    new changes in homepage, navebar

--- src/app/components/Slider.js
+++ src/app/components/Slider.js
@@ -6,61 +6,95 @@ import ortho from "@/app/assets/ortho.png";
 import Root from "@/app/assets/Root.png";
 import dentistry from "@/app/assets/dentistry.png";
 import Implants from "@/app/assets/Implants.png";
-
-const cardData = [
-  {
-    Image: care,
-    title: 'Exceptional Dental Care',
-    subtitle: 'Let the root canal dentists at Story Dental provide you with the care you need to alleviate tooth pain and to save your smile. A root canal will treat the diseased tissue (pulp) while preserving the rest of the tooth. Studies show that bacteria from a diseased root canal can cause almost any type of illness such as arthritis, heart disease, kidney disease and others.',
-  },
-  {
-    Image: dentistry,
-    title: 'General Dentistry',
-    subtitle: 'Choose our team at Story Dental as your general dentist in Irving. We are primary care dentists here to diagnose, treat and manage your oral health, including gum care, root canals, fillings, crowns, veneers, bridges, and preventive education.',
-  },
-  {
-    Image: ortho,
-    title: 'Orthodontics',
-    subtitle: 'Our orthodontic services in Irving focus on straighten your teeth, correcting your bite and malocclusion. We provide both traditional methods as well as Invisalign to give you the smile you’ve been waiting for on your time and budget.',
-  },
-  {
-    Image: Cosmetic,
-    title: 'Cosmetic Dentistry',
-    subtitle: 'Story Dental provides cosmetic dentistry in Irving. Our services primarily focus on improving dental aesthetics such as color, position, shape, size, alignment, and overall smile appearance. Smile with confidence. You deserve it.',
-  },
-  {
-    Image: Root,
-    title: 'Root Canals',
-    subtitle: 'Let the root canal dentists at Story Dental provide you with the care you need to alleviate tooth pain and to save your smile. A root canal will treat the diseased tissue (pulp) while preserving the rest of the tooth. Studies show that bacteria from a diseased root canal can cause almost any type of illness such as arthritis, heart disease, kidney disease and others.',
-  },
-  {
-    Image: Implants,
-    title: 'Dental Implants',
-    subtitle: 'Dental implants allow you to live the way you want to – confidently eating, smiling, laughing, talking, kissing, and enjoying all of your everyday activities without worrying about your teeth. Our Irving Dentists are dedicated to providing you with the strongest, most natural feeling smile. Dental implants are a great way to replace missing teeth.',
-  },
-];
+import { Container } from 'postcss';
 
 const Slider = () => {
   return (
-    <div className="container mx-auto p-4 h-[80vh]">
-      <section className="slider-section flex overflow-x-scroll space-x-4 h-full">
-        {cardData.map((card, index) => (
-          <div
-            key={index}
-            className="min-w-[100%] md:min-w-[40%] bg-white shadow-md rounded-lg p-4 flex flex-col items-center space-y-2"
-          >
-            <div className="text-4xl">
-              {typeof card.Image === 'string' ? (
-                card.Image
-              ) : (
-                <Image src={card.Image} alt={card.title} className="w-16 h-16" />
-              )}
-            </div>
-            <h2 className="text-xl font-semibold">{card.title}</h2>
-            <p className="text-gray-500">{card.subtitle}</p>
-          </div>
-        ))}
-      </section>
+    <div className="container ">
+       <div className='card'>
+        {/* <Image src="/Cosmetic.png"  width={50} height={50} /> */}
+        <Image
+          src={care}
+          alt="Cosmetic "
+          style={{ width: '30%', height: '30%' }}
+          className='card-img'
+        />
+        <div>
+          <h2 className='card-title'> Exceptional Dental Care</h2>
+          <p className='card-des'> Let the root canal dentists at Story Dental provide you with the care you need to alleviate tooth pain and to save your smile. A root canal will treat the diseased tissue (pulp) while preserving the rest of the tooth. Studies show that bacteria from a diseased root canal can cause almost any type of illness such as arthritis, heart disease, kidney disease and others. </p>
+        </div>
+
+      </div>
+      <div className='card'>
+        {/* <Image src="/Cosmetic.png"  width={50} height={50} /> */}
+        <Image
+          src={dentistry}
+          alt="Cosmetic "
+          style={{ width: '30%', height: '30%' }}
+          className='card-img'
+        />
+        <div>
+          <h2 className='card-title'> General Dentistry</h2>
+          <p className='card-des'> Choose our team at Story Dental as your general dentist in Irving. We are primary care dentists here to diagnose, treat and manage your oral health, including gum care, root canals, fillings, crowns, veneers, bridges, and preventive education.</p>
+        </div>
+
+      </div>
+      <div className='card'>
+        {/* <Image src="/Cosmetic.png"  width={50} height={50} /> */}
+        <Image
+          src={Cosmetic}
+          alt="Cosmetic "
+          style={{ width: '30%', height: '30%' }}
+          className='card-img'
+        />
+        <div>
+          <h2 className='card-title'> Cosmetic Dentistry</h2>
+          <p className='card-des'> Story Dental provides cosmetic dentistry in Irving. Our services primarily focus on improving dental aesthetics such as color, position, shape, size, alignment, and overall smile appearance. Smile with confidence. You deserve it. </p>
+        </div>
+
+      </div>
+      <div className='card'>
+        {/* <Image src="/Cosmetic.png"  width={50} height={50} /> */}
+        <Image
+          src={ortho}
+          alt="Cosmetic "
+          style={{ width: '30%', height: '30%' }}
+          className='card-img'
+        />
+        <div>
+          <h2 className='card-title'> Orthodontics</h2>
+          <p className='card-des'> Our orthodontic services in Irving focus on straighten your teeth, correcting your bite and malocclusion. We provide both traditional methods as well as Invisalign to give you the smile you’ve been waiting for on your time and budget. </p>
+        </div>
+
+      </div>
+      <div className='card'>
+        {/* <Image src="/Cosmetic.png"  width={50} height={50} /> */}
+        <Image
+          src={Root}
+          alt="Cosmetic "
+          style={{ width: '30%', height: '30%' }}
+          className='card-img'
+        />
+        <div>
+          <h2 className='card-title'> Root Canals</h2>
+          <p className='card-des'> Let the root canal dentists at Story Dental provide you with the care you need to alleviate tooth pain and to save your smile. A root canal will treat the diseased tissue (pulp) while preserving the rest of the tooth. Studies show that bacteria from a diseased root canal can cause almost any type of illness such as arthritis, heart disease, kidney disease and others.</p>
+        </div>
+
+      </div>
+      <div className='card'>
+        {/* <Image src="/Cosmetic.png"  width={50} height={50} /> */}
+        <Image
+          src={Implants}
+          alt="Cosmetic "
+          style={{ width: '30%', height: '30%' }}
+          className='card-img'
+        />
+        <div>
+          <h2 className='card-title'> Dental Implants</h2>
+          <p className='card-des'> Dental implants allow you to live the way you want to – confidently eating, smiling, laughing, talking, kissing, and enjoying all of your everyday activities without worrying about your teeth. Our Irving Dentists are dedicated to providing you with the strongest, most natural feeling smile. Dental implants are a great way to replace missing teeth. </p>
+        </div>
+
+      </div>
     </div>
   );
 };
--- src/app/links/patientDetails/page.js
+++ src/app/links/patientDetails/page.js
@@ -3,6 +3,10 @@ import Footer from '@/app/components/Footer';
 import Navbar from '@/app/components/Navbar';
 import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
+import banner1 from '@/app/assets/banner1.jpg';
+import Image from 'next/image';
+import ai from '@/app/assets/ai.jpg';
+
 
 const UserForm = () => {
   const router = useRouter()
@@ -28,8 +32,17 @@ const UserForm = () => {
   return (
     <div>
       <Navbar />
-      <div className="max-w-md mx-auto my-16 p-6 bg-white rounded-lg  shadow-md">
-        <h2 className="text-2xl font-bold mb-6 text-center">Patient Information Form</h2>
+      <div className=" mx-auto my-16 p-6 bg-white rounded-lg  shadow-md">
+        <h2 className="text-2xl font-bold mb-6 text-center text-teal-700">Patient Information Form</h2>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          {/* Image Section */}
+          <div className=" items-center justify-center rounded-lg p-4">
+            <Image src={ai}alt="Patient" className="max-w-full h-auto rounded-md" style={{height:'450px'}} />
+
+          </div>
+        
+
         <form onSubmit={handleSubmit} className="space-y-4">
           <div>
             <label className="block text-sm font-medium text-gray-700">Patient Name</label>
@@ -95,12 +108,13 @@ const UserForm = () => {
             <button
               type="submit"
               className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-md shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-75"
-              onClick={()=>router.push("/ThankYou")}
+              onClick={() => router.push("/ThankYou")}
             >
               Submit
             </button>
           </div>
         </form>
+        </div>
       </div>
       <Footer />
     </div>
